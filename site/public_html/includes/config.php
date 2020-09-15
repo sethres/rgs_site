d8f1f430ffd8c9cbff3ec0499c5ecec57d980b3b
Nic Lubbers <Nic@tkg.com>
2020-09-14 20:48:50 -0400

    Switch config back to original so I can see product data.

--- site/public_html/includes/config.php
+++ site/public_html/includes/config.php
@@ -1,29 +1,18 @@
 <?php
-
 $domain = $_SERVER['SERVER_NAME'] . $_SERVER['REQUEST_URI'];
 
+$dbServer = getenv('DB_HOST');
+$dbUser = getenv('DB_USER');
+$dbPw = getenv('DB_PASS');
+$dbName = getenv('DB_NAME');
 $rpp = 16;
 
-$dsn = "mysql:host=".getenv('DB_HOST').";dbname=".getenv('DB_NAME');
-$options = [
-    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
-    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-    PDO::ATTR_EMULATE_PREPARES   => false,
-];
-try {
-    $pdo = new PDO($dsn, getenv('DB_USER'), getenv('DB_PASS'), $options);
-} catch (PDOException $e) {
-    die("Connection Failed!".$e->getMessage());
-}
+$conn = new mysqli($dbServer, $dbUser, $dbPw, $dbName);
 
-/*if($conn->connect_error){
+if($conn->connect_error){
   die("Connection Failed!".$conn->connect_error);
-}*/
+}
 
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
-
-
-
-?>
